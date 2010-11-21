576369a73e94b50a017549004362dacf1c8a6bd8
Dylan Willis <willisdylan@gmail.com>
2010-11-21 19:47:48 +1000

    Initial commit.  Basic functionality.

---
+++ blenderscript.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append("/home/dylan/Downloads/bwhite-libfreenect-d55c379/python/")
+from freenect import *
+
+from Blender import *
+
+import Blender, meshtools
+import re, struct, StringIO
+
+import cv
+import numpy as np
+
+def drawmesh(mesh):
+	Blender.NMesh.PutRaw(mesh,"obj",1)
+	Blender.Redraw()
+
+def display(dev, data, timestamp):
+	print "Looping Now"
+	data -= np.min(data.ravel())
+	mesh = Blender.NMesh.GetRaw()
+	for y in range(0,480,10):
+		for x in range(0,640,10):
+			z = data[y,x]
+			zz = z
+			if(zz<1000):
+				xx = (x-320)*(zz-10)*0.0021  #This 'calibration' copied using Zephod's windows software - because it 'just works'
+				yy = (y-240)*(zz-10)*0.0021
+				zz-=200
+				zz*=-1
+				mesh.verts.append(Blender.NMesh.Vert(xx, yy, zz))
+	drawmesh(mesh)
+runloop(depth_cb_factory(display), lambda *x: None)
